1f93b5fee7cc4be64aa7d63d84d1b75f7c8b8b5c
Lucas Antoniale Callegari <lucascallegari93@gmail.com>
2016-06-29 13:28:12 -0300

    Update lucas_CREATE.sql

--- tables/lucas_CREATE.sql
+++ tables/lucas_CREATE.sql
@@ -1,5 +1,15 @@
 DROP TABLE DisciplinaPreReq;
 
+ CREATE DATABASE "Prograd"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Portuguese_Brazil.1252'
+       LC_CTYPE = 'Portuguese_Brazil.1252'
+       CONNECTION LIMIT = -1;
+
+
+
 CREATE TABLE DisciplinaPreReq
 (
 	Disciplina_codigo character varying(10) not null,
