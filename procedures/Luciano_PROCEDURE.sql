e9891803c9fa848923f0e27c12cabdcf08616124
lucianogfl1 <lucianogfl1@gmail.com>
2016-06-29 14:57:44 -0300

    Add files via upload

---
+++ procedures/Luciano_PROCEDURE.sql
@@ -0,0 +1,42 @@
+﻿--	Procedure para realizar inserções na tabela ConselhoCurso
+
+CREATE OR REPLACE FUNCTION insereConselhoCurso
+	(novo_representante character varying(20),
+	novo_id integer )
+RETURNS void AS $$
+BEGIN
+	INSERT INTO ConselhoCurso(representante,id)
+		VALUES (
+			novo_representante, 
+			novo_id 
+			
+		);
+END;
+$$ LANGUAGE plpgsql CALLED ON NULL INPUT;
+
+
+--	Procedure para realizar inserções na tabela PertenceCCP
+
+CREATE OR REPLACE FUNCTION inserePertenceCCP
+	(new_categoria character varying(20), 
+	new_periodo date,
+	new_Pessoa_rg character varying(9) ,
+	new_ConselhoCurso_id integer 
+)
+RETURNS void AS $$
+BEGIN
+	INSERT INTO PertenceCCP(categoria, periodo,Pessoa_rg,ConselhoCurso_id )
+		VALUES (
+			new_categoria , 
+			new_periodo ,
+			new_Pessoa_rg  ,
+			new_ConselhoCurso_id 
+			
+		);
+END;
+$$ LANGUAGE plpgsql CALLED ON NULL INPUT;
+
+
+
+
+
