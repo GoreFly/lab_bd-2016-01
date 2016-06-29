fd85096e2d67a87739a51a99cab6c8ea67561325
Arthur Munhoz <arthurmunhoz@hotmail.com>
2016-06-29 15:39:36 -0300

    Update arthur_VIEW.sql
    Insercao da VIEW B

--- views/arthur_VIEW.sql
+++ views/arthur_VIEW.sql
@@ -1,3 +1,14 @@
+--View B
+--Lista os RA's do estudantes que estagiam e as infos dos estagios de cada um
+
+CREATE OR REPLACE VIEW view_infosEstagio
+AS
+SELECT ra as "RA", nome as "Empresa", supEmpresa as "Sup. Empresa", supUniversidade as "Sup. Universidade"
+FROM Empresa, Estudante, Estagia
+WHERE Empresa.cnpj = Estagia.empresa_cnpj 
+AND Estudante.cpf = Estagia.estudante_cpf;
+
+--View L
 -- Mostra os nomes das empresas nas quais existem alunos estagiando
 
 CREATE OR REPLACE VIEW view_empresasAlunosEstagiam
