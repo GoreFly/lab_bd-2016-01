84f5f9ccbe77a73f08405bb24e334290ad1240e8
lucianogfl1 <lucianogfl1@gmail.com>
2016-06-29 16:34:04 +0000

    Add files via upload

---
+++ views/luciano_VIEW.sql
@@ -0,0 +1,9 @@
+﻿
+create or replace view Reuniao_Mes_Atual(id_reuniao, pauta,Data_Inicio)
+	as select numero , pauta ,dataInicio
+	from Reuniao
+	where dataInicio  BETWEEN date_trunc('month',current_date) AND  date_trunc('month',current_date) + INTERVAL'1 month' - INTERVAL'1 day';
+select * 
+from Reuniao_Mes_Atual;
+
+
