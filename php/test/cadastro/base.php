e58707b779e4144c6fc81f8e743664b6de0ca24b
Thalles Ferreira <thallesferreirag@gmail.com>
2016-07-01 01:40:54 -0300

    Create base.php

---
+++ php/test/cadastro/base.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <title>BD</title>
+
+  <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+	<a href="index.php"> <- Voltar</a>
+	<h1></h1>
+	<form method="POST" action="controlador.php">
+		
+		<input name='submit' type="submit" value="">
+	</form>
+</body>
+</html>
