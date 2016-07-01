06b2b13be10e8c51b27d67b461ad5d7665ad896a
Thalles Ferreira <thallesferreirag@gmail.com>
2016-07-01 15:47:32 -0300

    Add files via upload
    Falta separar as informações que cada categoria tem acesso

---
+++ php/test/estudante.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <title>BD</title>
+
+  <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+	<h1>Digra - Integrando Visões</h1>
+	<h2>Cadastros</h2>
+	<ul>
+		<li><a href="cadastro/cadastrarAtividadeComplementar.php">Cadastrar Atividade Complementar</a></li>
+		<li><a href="cadastro/cadastrarReconhecimentoDeCurso.php">Cadastrar Reconhecimento de Curso</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarVisita.php">Cadastrar Visita</a></li>
+			<li><a href="cadastro/cadastrarFase.php">Cadastrar Fase</a></li>
+		</ul>
+		<li><a href="cadastro/cadastrarPessoa.php">Cadastrar Pessoa</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarEstudante.php">Cadastrar Estudante</a></li>
+			<li><a href="cadastro/cadastrarDocente.php">Cadastrar Docente</a></li>
+			<li><a href="cadastro/cadastrarTecnicoAdiministrativo.php">Cadastrar Tecnico Adiministrativo</a></li>
+		</ul>
+		<li><a href="cadastro/cadastrarNucleoDocente.php">Cadastrar Nucleo Docente</a></li>
+		<li><a href="cadastro/cadastrarCampus.php">Cadastrar Campus</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarDepartamento.php">Cadastrar Departamento</a></li>
+		</ul>
+		<li><a href="cadastro/cadastrarCentro.php">Cadastrar Centro</a></li>
+		<li><a href="cadastro/cadastrarConselhoCurso.php">Cadastrar Conselho Curso</a></li>
+		<li><a href="cadastro/cadastrarCurso.php">Cadastrar Curso</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarProjetoPoliticoPedagogico.php">Cadastrar Projeto Politico Pedagogico</a></li>
+		</ul>
+		<li><a href="cadastro/cadastrarDisciplina.php">Cadastrar Disciplina</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarTurma.php">Cadastrar Turma</a></li>
+			<ul>
+				<li><a href="cadastro/cadastrarSala.php">Cadastrar Sala</a></li>
+			</ul>
+		</ul>
+		<li><a href="cadastro/cadastrarEmpresa.php">Cadastrar Empresa</a></li>
+		<li><a href="cadastro/cadastrarReuniao.php">Cadastrar Reuniao</a></li>
+		<ul>
+			<li><a href="cadastro/cadastrarCalendario.php">Cadastrar Calendario</a></li>
+			<ul>
+				<li><a href="cadastro/cadastrarEvento.php">Cadastrar Evento</a></li>
+				<li><a href="cadastro/cadastrarAtividade.php">Cadastrar Atividade</a></li>
+			</ul>
+			<li><a href="cadastro/cadastrarAta.php">Cadastrar Ata</a></li>
+		</ul>
+		<li><a href="cadastro/cadastrarPoloDistancia.php">Cadastrar Polo a Distancia</a></li>
+	</ul>
+	<h2>Visualizar Informações</h2>
+	<ul>
+		<li><a href="visualizar/visualizarAtividadeComplementar.php">Visualizar Atividade Complementar</a></li>
+	</ul>
+</body>
+</html>
