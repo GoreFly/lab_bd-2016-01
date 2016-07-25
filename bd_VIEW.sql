23de52427bd2ba9efea0967184534c7882374a2a
lucianogfl1 <lucianogfl1@gmail.com>
2016-07-24 23:35:56 -0300

    Update bd_VIEW.sql

--- bd_VIEW.sql
+++ bd_VIEW.sql
@@ -241,3 +241,18 @@ create or replace view vw_estudanteturmasdisciplinas as
 			  Cursa.Turma_id = Turma.id and
 			  Turma.Disciplina_codigo = Disciplina.codigo
 		order by Disciplina.codigo;
+
+
+--o usuário poderá ver  o nome completo do representante do conselho de Curso.
+
+create or replace view Reuniao_Mes_Atual
+	as select numero , pauta ,dataInicio
+	from Reuniao
+	where dataInicio  BETWEEN date_trunc('month',current_date) AND  date_trunc('month',current_date) + INTERVAL'1 month' - INTERVAL'1 day';
+
+
+--View retorna o nome completo do Represetante do Conselho de Curso
+create or replace view vw_NomeCompleto_Representante_ConselhoCurso as
+	select pre_nome || ' '|| meio_nome || ' '||ultimo_nome as "Nome Completo"
+		from ConselhoCurso, Pessoa
+		where Pessoa_rg = rg ;
