00bc20f438ed9306086259589d646c09409a94fe
GuilhermeToshimitsu <nori_gui@outlook.com>
2016-09-12 20:57:54 -0300

    Update GUILHERME_VIEW.sql

--- views/GUILHERME_VIEW.sql
+++ views/GUILHERME_VIEW.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW view_participaRC
 AS
 SELECT p.curso_codigo as "Cod Curso", r.pre_nome AS "Nome", r.sobre_nome AS "Sobrenome"
-FROM Pessoa AS p, participaRC AS r
+FROM Pessoa AS r, participaRC AS p
 WHERE r.rg = p.rg
 ORDER BY p.curso_codigo;
