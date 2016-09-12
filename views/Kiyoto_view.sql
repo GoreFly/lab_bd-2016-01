974d681eb915abef27e8c01cad2d997f87c590b4
Rafael Kiyoto <rafa.kiyoto@gmail.com>
2016-09-12 17:40:12 -0300

    Update Kiyoto_view.sql

--- views/Kiyoto_view.sql
+++ views/Kiyoto_view.sql
@@ -1,3 +1,31 @@
+--Listar todas as disciplinas
+CREATE OR REPLACE VIEW vw_Disciplina
+AS
+SELECT *
+FROM Disciplina AS d
+ORDER BY d.Codigo;
+
+--Listar todos os estudantes
+CREATE OR REPLACE VIEW vw_Estudante
+AS
+SELECT *
+FROM Estudante AS e
+ORDER BY e.RA;
+
+--Listar todos os polos a distancia
+CREATE OR REPLACE VIEW vw_PoloDistancia
+AS
+SELECT *
+FROM PoloDistancia AS p
+ORDER BY p.Nome;
+
+--Listar todos as fotos de polos a distancia
+CREATE OR REPLACE VIEW vw_FotoPolo
+AS
+SELECT *
+FROM FotoPolo AS f
+ORDER BY f.Num_foto;
+
 --Listar Pós graduandos
 CREATE OR REPLACE VIEW vw_AlunosPosComp
 AS 
